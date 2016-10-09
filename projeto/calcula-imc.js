e6f822a18dc5939f1446552ff1b6a530a039ae3f
Bernardo Vieira <bvieira@gmail.com>
2016-10-08 23:38:28 -0300

    Finalizada aula 5

--- projeto/calcula-imc.js
+++ projeto/calcula-imc.js
@@ -1,40 +1,27 @@
-// var tdPeso = document.getElementById("peso-2");
-// var tdAltura = document.getElementById("altura-2");
-
-// var peso = tdPeso.textContent;
-// var altura = tdAltura.textContent; //testando com 0
-
-// var paciente = { "peso": peso, "altura": altura }
-
-// if (paciente.altura != 0) {
-//     var imc = paciente.peso / (paciente.altura * paciente.altura);
-//     document.getElementById("imc-2").textContent = imc;
-//     console.log(imc);
-// } else {
-//     console.log("Não posso executar uma divisão por 0!");
-// }
-
 var trPacientes = document.getElementsByClassName("paciente"); //Array de trs
 
+percorreArray(trPacientes, function (trPaciente) {
+
+    var pacienteAtual = montaPaciente(trPaciente);
+    var imc = pacienteAtual.pegaImc();
+
+    var tdImc = trPaciente.getElementsByClassName("info-imc")[0];
+    tdImc.textContent = imc;
+
+    console.log(imc);
+});
+
+
+/*
 for (var posicaoAtual = 0; posicaoAtual <= trPacientes.length - 1; posicaoAtual++) {
     var pacienteTr = trPacientes[posicaoAtual];
-    var tdNome = pacienteTr.getElementsByClassName("info-nome")[0];
-    var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0];
-    var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
+    var pacienteAtual = montaPaciente(pacienteTr);
 
-    var paciente = { nome: tdNome.textContent, peso: tdPeso.textContent, altura: tdAltura.textContent };
+    var imc = pacienteAtual.pegaImc();
 
-    if (paciente.altura != 0) {
+    var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
+    tdImc.textContent = imc;
 
-        var imc = paciente.peso / (paciente.altura * paciente.altura);
-
-        var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
-        tdImc.textContent = imc;
-
-        console.log(imc);
-
-    } else {
-
-        console.log("Não posso executar uma divisão por 0!");
-    }
+    console.log(imc);
 }
+*/
